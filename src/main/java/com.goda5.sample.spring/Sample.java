26121ddf1297dbf9a8952a9bdde1713f862a716d
chentongtc@gmail.com <chentongtc@gmail.com>
2016-05-14 20:17:15 +0100

    kafka client (producer/consumer).

--- src/main/java/com.goda5.sample.spring/Sample.java
+++ src/main/java/com.goda5.sample.spring/Sample.java
@@ -1,17 +1,54 @@
 package com.goda5.sample.spring;
 
+import com.google.common.collect.Lists;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Properties;
+
 @RestController
 @EnableAutoConfiguration
 public class Sample {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    @RequestMapping("/produce")
+    String produce() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        KafkaProducer producer = new KafkaProducer(props);
+        ProducerRecord<String,String> producerRecord = new ProducerRecord("sampletopic", "samplekey", "samplevalue");
+        producer.send(producerRecord);
+
+        return "producer";
+    }
+
+    @RequestMapping("/consume")
+    String consume() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "consumer-tutorial");
+        props.put("key.deserializer", StringDeserializer.class.getName());
+        props.put("value.deserializer", StringDeserializer.class.getName());
+        KafkaConsumer<String, String> consumer = new KafkaConsumer(props);
+        consumer.listTopics();
+        consumer.subscribe(Lists.newArrayList("sampletopic"));
+        ConsumerRecords<String, String> records = consumer.poll(1000);
+        for (ConsumerRecord<String, String> record : records) {
+            System.out.println(record.offset() + ": " + record.value());
+        }
+        return "consumer";
     }
 
     public static void main(String[] args) throws Exception {
