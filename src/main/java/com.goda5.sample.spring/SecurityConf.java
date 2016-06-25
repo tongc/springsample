171f110830764079176e6afde5fc646dd5e13927
chentongtc@gmail.com <chentongtc@gmail.com>
2016-06-25 17:39:27 +0100

    togglz configurations.

---
+++ src/main/java/com.goda5.sample.spring/SecurityConf.java
@@ -0,0 +1,34 @@
+package com.goda5.sample.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by tong on 25/06/2016.
+ */
+@Configuration
+public class SecurityConf extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("ADMIN");
+    }
+}
