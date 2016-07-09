4666a4e017e0a98e4568001a598d82780f523931
chentongtc@gmail.com <chentongtc@gmail.com>
2016-07-09 18:57:46 +0100

    launch java

--- src/main/java/com.goda5.sample.spring/MavenInvoker.java
+++ src/main/java/com.goda5.sample.spring/MavenInvoker.java
@@ -4,10 +4,11 @@ import com.jcabi.aether.Aether;
 import org.apache.maven.shared.invoker.*;
 import org.sonatype.aether.artifact.Artifact;
 import org.sonatype.aether.repository.RemoteRepository;
-import org.sonatype.aether.resolution.DependencyResolutionException;
 import org.sonatype.aether.util.artifact.DefaultArtifact;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -16,8 +17,9 @@ import java.util.Collections;
  * Created by tong on 09/07/2016.
  */
 public class MavenInvoker {
-    public static void main(String[] args) throws DependencyResolutionException {
-        File local = new File("c:/tmp/local-repository");
+    public static void main(String[] args) throws Exception {
+        org.eclipse.jetty.server.Server.main();
+        File local = new File("C:\\Users\\tong\\.m2\\repository");
         Collection<RemoteRepository> remotes = Arrays.asList(
                 new RemoteRepository(
                         "maven-central",
@@ -25,12 +27,35 @@ public class MavenInvoker {
                         "http://repo1.maven.org/maven2/"
                 ));
         Collection<Artifact> deps = new Aether(remotes, local).resolve(
-                new DefaultArtifact("junit", "junit-dep", "", "jar", "4.10"),
+                new DefaultArtifact("org.apache.openejb", "tomee-embedded", "", "jar", "1.7.4"),
                 "runtime"
         );
 
+        String command = "java -cp " + deps.stream().findFirst().get().getFile().getAbsolutePath() + " org.apache.tomee.embedded.Main";
+        Process exec = Runtime.getRuntime().exec("java");
+        exec.waitFor();
+
+        StringBuffer sb = new StringBuffer();
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(exec.getErrorStream()));
+        String line = "";
+        while ((line = reader.readLine())!= null) {
+            sb.append(line + "\n");
+        }
+        System.out.println(sb.toString());
+
+        String javaCommand = System.getProperty("java.home") + "/bin/java";
+        ProcessBuilder processBuilder = new ProcessBuilder(javaCommand, "-cp", deps.stream().findFirst().get().getFile().getAbsolutePath(), "org.apache.tomee.embedded.Main");
+        Process process = processBuilder.start();
+        process.waitFor();
+        reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        while ((line = reader.readLine())!= null) {
+            sb.append(line + "\n");
+        }
+        System.out.println(sb.toString());
+
         InvocationRequest request = new DefaultInvocationRequest();
-        request.setPomFile(new File("/path/to/pom.xml"));
+        request.setPomFile(new File("c:/tmp/local-repository"));
         request.setGoals(Collections.singletonList("install"));
 
         Invoker invoker = new DefaultInvoker();
@@ -41,5 +66,12 @@ public class MavenInvoker {
         } catch (MavenInvocationException e) {
             e.printStackTrace();
         }
+
+        System.out.println(execCmd("java"));
+    }
+
+    public static String execCmd(String cmd) throws java.io.IOException {
+        java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getErrorStream()).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
