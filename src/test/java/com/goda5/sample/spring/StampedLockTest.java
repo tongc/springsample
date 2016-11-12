1fb2d24f1c81ccf056d7c42d39d1c50217f3d325
chentongtc@gmail.com <chentongtc@gmail.com>
2016-11-12 09:50:09 +0000

    stamped lock

--- src/test/java/com/goda5/sample/spring/StampedLockTest.java
+++ src/test/java/com/goda5/sample/spring/StampedLockTest.java
@@ -1,21 +1,30 @@
 package com.goda5.sample.spring;
 
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.StampedLock;
 
 public class StampedLockTest {
     private static final StampedLock lock = new StampedLock();
     private static final StampedLock lock2 = new StampedLock();
+    private static final ReentrantLock lock3 = new ReentrantLock();
 
     public static void main(String[] args) {
-        lock.readLock();
-        lock2.writeLock();
+        System.out.println(lock.readLock());
         System.out.println(lock.tryUnlockRead());
         System.out.println(lock.tryReadLock());
+        System.out.println(lock.tryConvertToWriteLock(257));
         System.out.println(lock.tryWriteLock());
-        System.out.println(lock2.tryUnlockWrite());
         System.out.println(lock.isReadLocked());
         System.out.println(lock.isWriteLocked());
+        System.out.println(lock.tryUnlockWrite());
+
+        System.out.println(lock2.tryWriteLock());
+        System.out.println(lock2.tryReadLock());
+        System.out.println(lock2.tryWriteLock());
         System.out.println(lock2.isReadLocked());
         System.out.println(lock2.isWriteLocked());
+
+        lock3.tryLock();
     }
+
 }
